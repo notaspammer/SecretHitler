6f0ba89bc2d943075ddc50d1a3dd74f08bfe1de0
Thomas Cole <thomascole@carolina.rr.com>
2017-01-20 16:49:56 -0500

    Added some stuff

---
+++ PolicyCard.js
@@ -0,0 +1,8 @@
+var cardHeight = 10;
+var cardWidth = 4;
+
+class PolicyCard{
+  constructor(){
+
+  }
+}
--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+var canvas = document.getElementById("gC");  //The gamespace where we can draw
+var ctx = canvas.getContext("2d"); //the render context of the gameCanvas
+
+function drawCard(posX, posY){
+  ctx.beginPath();
+  ctx.rect(posX, posY, 40, 50);
+  ctx.fillStyle = "#FF0000";
+  ctx.fill();
+  ctx.closePath();
+}
+
 function playTurn(){
   var x = document.getElementById("Debug");
   x.innerHTML = "You clicked on the Next Turn button"
